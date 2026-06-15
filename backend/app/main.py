7614d50fa1f72d126addceb7e16deddce3d34c4c
noryaa1968-art <noryaa1968@gmail.com>
2026-06-15 21:46:12 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,20 +14,7 @@ app = FastAPI(title=PROJECT_NAME)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
-        "http://localhost:5175",
-        "http://127.0.0.1:5175",
-        "http://localhost:5176",
-        "http://127.0.0.1:5176",
-        "http://localhost:8000",
-        "http://127.0.0.1:8000",
-        "https://refactored-space-giggle-p7jjwrr9gw9rc7r4j-5175.app.github.dev",
-        "https://refactored-space-giggle-p7jjwrr9gw9rc7r4j-8000.app.github.dev",
-    ],
+    allow_origins=["*"],
     allow_origin_regex=r"https://.*\.app\.github\.dev",
     allow_credentials=True,
     allow_methods=["*"],
