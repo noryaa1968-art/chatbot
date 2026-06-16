9cbf96ceada490b430b95b3dda0adc82aab60d97
noryaa1968-art <noryaa1968@gmail.com>
2026-06-16 16:31:45 +0530

    Update connection.py

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -2,9 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from ..core.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL,
-                       connect_args={"check_same_thread": False}
-                       )
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(
     autocommit=False, 
